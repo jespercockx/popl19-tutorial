6a13304946842f6d0c6a35c0ce8426812b1a234f
Jesper Cockx <jesper@sikanda.be>
2019-01-14 01:17:27 +0100

    More improvements to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# install and check dependencies:
-sudo apt-get install git cabal-install emacs || true
-cabal --version || exit 1
-git   --version || exit 1
-emacs --version || exit 1
+# check or install dependencies:
+cabal --version || sudo apt-get install cabal-install || exit 1
+git   --version || sudo apt-get install git           || exit 1
+emacs --version || sudo apt-get install emacs         || exit 1
+sudo apt-get install zlib1g-dev
 
 # install Agda (requires GHC):
 cabal update
@@ -13,9 +13,7 @@ cabal install Agda && agda-mode setup
 
 # install standard library (requires git):
 git clone https://github.com/agda/agda-stdlib.git
-cd agda-stdlib
-git checkout v0.17
-cabal install
+cd agda-stdlib && git checkout v0.17 && cabal install
 mkdir $HOME/.agda
 echo $PWD/agda-stdlib/standard-library.agda-lib >> $HOME/.agda/libraries
 echo standard-library >> $HOME/.agda/defaults
